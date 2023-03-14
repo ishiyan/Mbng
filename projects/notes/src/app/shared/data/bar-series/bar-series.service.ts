76958f5471980c8e11a82214adba4f0ef052caf7
ivas <ivas@saxobank.com>
2023-03-14 14:44:43 +0100

    new data

--- projects/notes/src/app/shared/data/bar-series/bar-series.service.ts
+++ projects/notes/src/app/shared/data/bar-series/bar-series.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 
-import { barSeriesTalib1d } from './bar-series-data/bar-series-talib-1d';
+// import { barSeriesTalib1d } from './bar-series-data/bar-series-talib-1d';
+import { barSeriesRcoXpar1dSmall } from './bar-series-data/bar-series-rco-xpar-1d-small';
+import { barSeriesMbwsXpar1dSmall } from './bar-series-data/bar-series-mbws-xpar-1d-small';
+import { barSeriesOrXpar1d } from './bar-series-data/bar-series-or-xpar-1d';
+import { barSeriesCdiXpar1d } from './bar-series-data/bar-series-cdi-xpar-1d';
 import { barSeriesRdsaXams1d } from './bar-series-data/bar-series-rdsa-xams-1d';
 import { barSeriesRdsaXams1m } from './bar-series-data/bar-series-rdsa-xams-1m';
 import { RemovableSeries } from '../removable-series.interface';
@@ -11,7 +15,10 @@ import { SeriesService } from '../abstractions/series.service';
 })
 export class BarSeriesService extends SeriesService {
   protected seriesArray: RemovableSeries[] = [
-    barSeriesTalib1d,
+    barSeriesRcoXpar1dSmall,
+    barSeriesMbwsXpar1dSmall,
+    barSeriesOrXpar1d,
+    barSeriesCdiXpar1d,
     barSeriesRdsaXams1d,
     barSeriesRdsaXams1m
   ];
